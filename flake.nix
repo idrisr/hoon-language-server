c415cd0336ff290bb7a8e104d17132f1d21910a7
Idris Raja <idris.raja@gmail.com>
2023-07-19 14:07:53 -0500

    attempt overlay

--- flake.nix
+++ flake.nix
@@ -1,29 +1,27 @@
 {
   description = "LSP bridge for Hoon";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
 
-    parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-  };
-
-  outputs = inputs @ {
-    self
-    , nixpkgs
-    , parts
-  }: parts.lib.mkFlake { inherit inputs; } {
-    systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-
-    perSystem = { pkgs, system, ... }: {
-      packages.hoon-language-server = pkgs.callPackage ./. { };
-      packages.default = self.packages.${system}.hoon-language-server;
-      apps.default = {
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system} = rec {
+        hoon-language-server = pkgs.callPackage ./. { };
+        default = hoon-language-server;
+      };
+      apps.${system}.default = {
         type = "app";
-        program = "${self.packages.${system}.hoon-language-server}/bin/hoon-language-server";
+        program = "${
+            self.packages.${system}.hoon-language-server
+          }/bin/hoon-language-server";
+      };
+      overays = {
+        hoon-language-server = final: prev: {
+          inherit (self.packages) hoon-language-server;
+        };
       };
     };
-  };
 }
